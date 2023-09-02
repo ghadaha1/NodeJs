23f422c0f9c3e7207edc4f26c89f56e495602253
ghada <ghadahamoudi737@gmail.com>
2023-09-02 12:56:46 +0100

    Create Requests

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const express = require('express');
 
 const app = express();
 
+app.post( '/add',()=>{
+    console.log("add work");
+});
 
+app.get('/getall', ()=>{
+    console.log('get work');
+});
 
+app.put('/update', ()=>{
+    console.log('update work');
+});
 
+app.delete('/delete', ()=>{
+    console.log('delete work');
+});
 
 app.listen(3000,()=>{
     console.log('server work')
